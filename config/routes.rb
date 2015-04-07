07c258a38420f9f248d2877e4c9fbd68b7aa2c8b
boro <roboboro@mail.ru>
2015-04-08 01:19:19 +0300

    moarmoarq

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  resources :conversations
-
-  resources :messages
-
+  
   root 'home#index'
   resources :posts
   devise_for :users, controllers: {registrations: "registrations"}
