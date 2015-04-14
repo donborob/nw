866ba1028bd0d318775bb6c933b613e0a22eea30
boro <roboboro@mail.ru>
2015-04-14 14:44:01 +0300

    forms were replaced by parials

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150412204219) do
+ActiveRecord::Schema.define(version: 20150414113240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 20150412204219) do
   create_table "conversations", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
     t.integer  "partner_id"
+    t.integer  "user_id"
   end
 
   create_table "likes", force: true do |t|
